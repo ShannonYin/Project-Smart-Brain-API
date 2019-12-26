11434c02cc744a9b3dd74e281c646e1104d73b2a
JeanYin <yinxiaohan@outlook.com>
2019-12-26 15:37:47 -0600

    adding connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client:'pg',
 	connection:{
-		host:'postgresql-contoured-85492',
-		user:'yinxi',
-		password:'yxh41012',
-		database:'smart-brain'
+		host:process.env.DATABASE_URL,
+		ssl:true,
 	}
 });
 
