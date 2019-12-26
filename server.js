dd9d238722a37b792eeaec189b8e3f5f5958bc4e
JeanYin <yinxiaohan@outlook.com>
2019-12-26 15:36:04 -0600

    updating db

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client:'pg',
 	connection:{
-		host:'127.0.0.1',
+		host:'postgresql-contoured-85492',
 		user:'yinxi',
 		password:'yxh41012',
 		database:'smart-brain'
